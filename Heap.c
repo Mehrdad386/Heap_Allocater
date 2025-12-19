5a4a0924f3e6ec3ab78cc361d9b577237ad8af1f
Mehrdad <mehrdadmrs10@gmail.com>
2025-12-20 01:29:38 +0330

    halloc function implemented completely

--- Heap.c
+++ Heap.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <errno.h>
+#include<windows.h>
 
 struct chunk_t{
     uint32_t size;
@@ -51,10 +52,48 @@ int hinit (struct heap_t* h , void *mem , uint32_t size){
     h->start = first ; //attach the first chunk to heap structure
     h->avail = first->size ; // initialize available memory counter (this value decrease in allocation and increase in deaallocation)
 
-
 }
 
 void *halloc (struct heap_t* h , size_t size){
+
+    struct chunk_t *chunk ;
+    size_t total_size ;
+    void *mem ;
+    
+    //the size of zero is not a valid allocation request
+    if(size ==0){
+        errno = EINVAL ; //invalid request
+        return NULL ;
+    }
+
+    total_size = size + sizeof(struct chunk_t) ; //total memory required
+
+    //we alloc memory using virtual alloc insteaad of mmap because we're on windows and have no access to mmap
+    mem = VirtualAlloc(
+        NULL, //IP address = null it means let the system choose the address
+        total_size, //dwSize = total_size
+        MEM_RESERVE | MEM_COMMIT, // f allocaation type : MEM_RESERVED : reserve the address space , MEM_COMMIT : allocates physical pages
+        PAGE_READWRITE //flProtect : PAGE_READWRITE : memory is readable and writable
+    );
+
+    //check for allocation failure
+    //virtualAlloc returns NULL on failure
+    if(mem = NULL){
+        errno = ENOMEM ; // not enough memory
+        return NULL ;
+    }
+
+    //Treat the begginning of allocated memory as chunk metadata
+    chunk = (struct chunk_t *) mem ; //type casting
+
+    //initialization metadata fields
+    chunk->size = size ; //number of bytes requested by user
+    chunk->inuse = 1 ; //it is in use
+    chunk->next = NULL ; //this chunk is standalone for now
+
+    //return pointer to payload area , hiding metadata from user
+    //we add size of chunk to the base address to skip metadata
+    return (void *)((uint8_t *)mem + sizeof(struct chunk_t)) ;
 
 }
 
